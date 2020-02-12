f5ffaea7cb72614da66c3299844d8ac7d99ad550
suy427 <sondahum@gmail.com>
2020-02-12 16:55:45 +0900

    forget this git...TT

--- test-dahum/src/main/java/com/sondahum/javadahum/testdahum/spring/AbstractTestHelper.java
+++ test-dahum/src/main/java/com/sondahum/javadahum/testdahum/spring/AbstractTestHelper.java
@@ -1,10 +1,9 @@
 package com.sondahum.javadahum.testdahum.spring;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestStarter.class)
